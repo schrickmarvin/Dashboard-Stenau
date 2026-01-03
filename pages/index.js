3af70656c7a3e847012e614b17e27c106c0bf904
schrickmarvin <schrickmarvin@googlemail.com>
2026-01-03 07:42:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-import Dashboard from "./dashboard";
+// pages/index.js
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  return <Dashboard />;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/dashboard");
+  }, [router]);
+
+  return (
+    <div style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>Dashboard Stenau</h1>
+      <p>Weiterleitung…</p>
+    </div>
+  );
 }
